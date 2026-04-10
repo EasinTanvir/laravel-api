43af804058f7cc66ad06e4b7ccb496dc4fcb13dd
EasinTanvir <easintanvir102@gmail.com>
2026-04-10 23:15:16 +0600

    added routes with vesrion

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Post\PostController;
+use App\Http\Controllers\Api\V1\User\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+
+Route::get('/user',[UserController::class, "index"])->name("all user");
+
+
+Route::prefix('v1')->group(function(){
+
+  Route::apiResource("posts", PostController::class);
+
+});
