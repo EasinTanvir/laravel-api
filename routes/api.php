0905f74b8d4c828e5bce60222ecb582eefe0753d
EasinTanvir <easintanvir102@gmail.com>
2026-04-11 16:46:24 +0600

    added laravel breeze

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Api\V1\User\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
@@ -26,3 +26,6 @@ Route::prefix('v1')->group(function(){
   Route::apiResource("posts", PostController::class);
 
 });
+
+
+require __DIR__.'/auth.php';
