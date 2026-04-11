733d289dc8623177fc24311d272b193fa109ee1a
EasinTanvir <easintanvir102@gmail.com>
2026-04-11 19:26:43 +0600

    added login system

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,22 +4,31 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class LoginController extends Controller
 {
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): Response
+    public function store(LoginRequest $request)
     {
         $request->authenticate();
+        $user = $request->user();
+        $token = $user->createToken('main')->plainTextToken;
 
-        $request->session()->regenerate();
+        // $request->session()->regenerate();
 
-        return response()->noContent();
+        return [
+
+        'user' => new UserResource($user),
+        'token'=> $token,
+
+        ];
     }
 
     /**
@@ -27,11 +36,9 @@ class LoginController extends Controller
      */
     public function destroy(Request $request): Response
     {
-        Auth::guard('web')->logout();
+         $user = $request->user();
+         $user->currentAccessToken()->delete();
 
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
 
         return response()->noContent();
     }
